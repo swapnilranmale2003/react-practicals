498e9bad34c6b8565b4732bf920fd94196b37e6a
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-03-11 00:57:32 +0530

    toast position

--- react-notification/src/App.js
+++ react-notification/src/App.js
@@ -1,19 +1,29 @@
-import { ToastContainer,toast} from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 
-
-
 function App() {
-  const notify = () =>
-  {
-     toast("Wow so easy!");
+  const notify = () => {
+    toast('Hello Geeks 4',{
+      position: "top-center"
+    });
+    toast.error('Hello',{
+      position:"top-left"
+    })
+    toast.success('Logged in',{
+      position:"bottom-left"
+    })
+
+    toast.info('Info Notification',{
+      position:"bottom-right"
+    })
   }
+
   return (
     <div className="App">
-     <h1>React Notification</h1>
-     <button onClick={notify}>Notify!</button>
-     <ToastContainer />
+      <h1>React Notification</h1>
+      <button onClick={notify}>Notify!</button>
+      <ToastContainer />
     </div>
   );
 }
