a9dd13875b52eb6158fb667036e59feca11e624e
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-03-11 01:14:18 +0530

    closeToast as a props

--- react-notification/src/App.js
+++ react-notification/src/App.js
@@ -1,21 +1,37 @@
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
-
+const CustomToast = ({closeToast})=>{
+return(
+  <div>
+    Something went wrong
+    <button onClick={closeToast}>Close</button>
+  </div>
+)
+}
 function App() {
   const notify = () => {
-    toast('Hello Geeks 4',{
-      position: "top-center"
+    toast('Hello Geeks 4', {
+      position: "top-center",
+      autoClose: 1000
     });
-    toast.error('Hello',{
-      position:"top-left"
+    toast.error('Error', {
+      position: "top-left",
+      autoClose:8000
     })
-    toast.success('Logged in',{
-      position:"bottom-left"
-    })
+    toast.success('Logged in', {
+      position: "bottom-left"  
+    }) // default auto close time is 5sec!
 
-    toast.info('Info Notification',{
-      position:"bottom-right"
+    toast.info('Info Notification', {
+      position: "bottom-right",
+      autoClose:false
+    })
+    toast.warning(<CustomToast />, {
+      position: "bottom-center"
+    })
+    toast('top right',{
+      position:"top-right"
     })
   }
 
