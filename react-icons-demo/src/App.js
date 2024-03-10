cecda113b26b020c0f4ecea00453e6223ad54e6b
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-03-10 23:41:17 +0530

    toast notification

--- react-icons-demo/src/App.js
+++ react-icons-demo/src/App.js
@@ -2,14 +2,17 @@ import './App.css';
 import { IconContext } from 'react-icons';
 import { FaReact } from "react-icons/fa";
 import { MdAccessAlarm } from "react-icons/md";
-
+import { MdAdb } from "react-icons/md";
 function App() {
   return (
-    <IconContext.Provider value={{color:'blue', size:'10rem'}} >
-    <div className="App">
-     <FaReact/> {/* 10rem = 160px */}
-     <MdAccessAlarm/>
-    </div>
+    <IconContext.Provider value={{ color: 'blue', size: '10rem' }} >
+      {/* if you want to maintain same color and size to react icons then use inside IconContext Provider */}
+      <div className="App">
+        <FaReact /> {/* 10rem = 160px */}
+        <MdAccessAlarm />
+        <MdAdb color='purple' size={'20rem'} />
+
+      </div>
     </IconContext.Provider>
   );
 }
