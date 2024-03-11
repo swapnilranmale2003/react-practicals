d2fd7f803183ffee12fb22e34867467ed28233d8
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-03-11 18:19:40 +0530

    added props to Tippy component

--- react-tooltip/src/App.js
+++ react-tooltip/src/App.js
@@ -1,32 +1,48 @@
 import './App.css';
 import Tippy from '@tippy.js/react';
 import 'tippy.js/dist/tippy.css';
+import React, { forwardRef } from 'react'
+
+
 // component
-const ColoredComponent = ()=>{
-  return(
+const ColoredComponent = () => {
+  return (
     <p>Colored Component</p>
   )
 }
+const CustomChild=forwardRef((props, ref)=>{
+  return(
+    <div ref={ref}>
+      <div>first line</div>
+      <div>second line</div>
+    </div>
+  )
+})
 function App() {
   return (
     <div className="App">
       <h1>React tooltip project demo</h1>
-      <Tippy content={"Hello"}>
+      <Tippy placement='right' arrow={false} delay={1000} content={"Hello"}>
         <button>Hover</button>
       </Tippy>
-      <div style={{paddingTop:"20px"}}>
-        <Tippy  content={<span style={{color:"orange"}}>Paragraph by lorem</span>}>
+      <div style={{ paddingTop: "20px" }}>
+        <Tippy content={<span style={{ color: "orange" }}>Paragraph by lorem</span>}>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, praesentium!</p>
         </Tippy>
       </div>
       <div>
-        <Tippy  content={<p  style={{color:"red"}}>Nature Glimpsee!</p>} >
+        <Tippy content={<p style={{ color: "red" }}>Nature Glimpsee!</p>} >
           <img src="assets/nature.jpeg" alt="" />
         </Tippy>
       </div>
-      <div style={{paddingTop:"20px"}}>
-        <Tippy  content={<ColoredComponent />}>
-       <button>Hover</button>
+      <div style={{ paddingTop: "20px" }}>
+        <Tippy content={<ColoredComponent />}>
+          <button>Hover</button>
+        </Tippy>
+      </div>
+      <div style={{ paddingTop: "20px" }}>
+        <Tippy content={<ColoredComponent />}>
+        <CustomChild></CustomChild>
         </Tippy>
       </div>
     </div>
