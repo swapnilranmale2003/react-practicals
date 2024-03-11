7878dfbcd91f2d8d5aeb294d3ba490be0290c820
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-03-11 17:31:08 +0530

    Basic simple example of tooltip

--- react-tooltip/src/App.js
+++ react-tooltip/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Tippy from '@tippy.js/react';
+import 'tippy.js/dist/tippy.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>React tooltip project demo</h1>
+     <Tippy content={"Hello"}>
+     <button>Hover</button>
+     </Tippy>
     </div>
   );
 }
