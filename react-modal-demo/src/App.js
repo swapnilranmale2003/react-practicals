b032157b2cd21b098ab17c0b9d8311f009a5b9e5
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-03-11 17:15:42 +0530

    default style added to modal as a overlay & content

--- react-modal-demo/src/App.js
+++ react-modal-demo/src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
 import Modal from 'react-modal'
 import React, { useState } from 'react'
-
+Modal.setAppElement('#root')
 function App() {
   const [ModalIsOpen, setModalIsOpen] = useState(false);
   return (
     <div className="App">
       <button onClick={() => setModalIsOpen(true)}>Open Modal</button>
-      <Modal isOpen={ModalIsOpen} onRequestClose={()=>setModalIsOpen(false)}>
+      <Modal isOpen={ModalIsOpen} shouldCloseOnOverlayClick={false} onRequestClose={()=>setModalIsOpen(false)}
+      style={
+        {
+          overlay:{
+            backgroundColor:"gray"
+          },
+          content:{
+            color:"orange"
+          }
+        }
+        
+      }
+      >
         <h1>Modal Title</h1>
         <p>Modal Body</p>
         <div>
