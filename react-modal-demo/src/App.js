a41b0fed634216ee76620c695fc8974da83ee115
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-03-11 16:57:06 +0530

    Basic MOdal is working fine

--- react-modal-demo/src/App.js
+++ react-modal-demo/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Modal from 'react-modal'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Modal isOpen={true}>
+      <h1>Modal Title</h1>
+      <p>Modal Body</p>
+     </Modal>
     </div>
   );
 }
