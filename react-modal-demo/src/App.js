1d5f8576abdaaff5ae5d984304bc97d3f8ef9b82
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-03-11 17:06:59 +0530

    open/close model using useState hooks with isOpen onRequestClose props!

--- react-modal-demo/src/App.js
+++ react-modal-demo/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import Modal from 'react-modal'
+import React, { useState } from 'react'
+
 function App() {
+  const [ModalIsOpen, setModalIsOpen] = useState(false);
   return (
     <div className="App">
-     <Modal isOpen={true}>
-      <h1>Modal Title</h1>
-      <p>Modal Body</p>
-     </Modal>
+      <button onClick={() => setModalIsOpen(true)}>Open Modal</button>
+      <Modal isOpen={ModalIsOpen} onRequestClose={()=>setModalIsOpen(false)}>
+        <h1>Modal Title</h1>
+        <p>Modal Body</p>
+        <div>
+          <button onClick={() => setModalIsOpen(false)}>Close Modal</button>
+        </div>
+      </Modal>
     </div>
   );
 }
