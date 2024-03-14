34fba5f82d63161765a8df51829688ab9976a482
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-03-14 23:15:23 +0530

    my-app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,10 @@ import './App.css';
 import Hello from './component/Hello';
 
 function App() {
+ 
   return (
     <div className="App">
-      <Hello name="Adam" mobile='1234567890' />
-      <Hello name="Martin" />
-      <Hello name="Apple" />
-      <Hello name="Mango" />
+     <Hello />
     </div>
   );
 }
