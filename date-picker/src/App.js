c662f5fb6e8bec8017b513dc58cf411d051023f5
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-03-28 16:22:36 +0530

    added props to DATEPICKER component

--- date-picker/src/App.js
+++ date-picker/src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
 import './App.css';
-
+import 'react-datepicker/dist/react-datepicker.css';
 function App() {
+  const [selectedDate, setSelectedDate] = useState(null);
   return (
     <div className="App">
-     <h1>Date Picker</h1>
+      <h1>Date Picker</h1>
+      <DatePicker
+        selected={selectedDate}
+        onChange={date => setSelectedDate(date)}
+        // dateFormat={'yyyy/MM/dd'}
+        dateFormat={'dd/MM/yyyy'}
+        // minDate={new Date()}
+        // maxDate={new Date()}
+        placeholderText='Pick DOB'
+        filterDate={date=>date.getDay() !== 6 && date.getDay() !== 0}
+        isClearable
+        showMonthDropdown
+        showYearDropdown
+        scrollableYearDropdown
+      />
     </div>
   );
 }
