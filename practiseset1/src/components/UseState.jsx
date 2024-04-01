17393ad094d0dabf13120aea5e40ba4fe1900ef5
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-04-01 18:05:02 +0530

    useState Hooks

--- practiseset1/src/components/UseState.jsx
+++ practiseset1/src/components/UseState.jsx
@@ -1,11 +1,11 @@
-
 import React, { useState } from "react";
 function UseState() {
   const [count, setCount] = useState(0);
   return (
+    
     <div>
       <h1>useState Hook {count}</h1>
-      <button onClick={()=>setCount(count+1)}>Increment</button>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
     </div>
   );
 }
