4021f57cb1381b4664f60641d854dd5912448c42
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-04-01 18:05:20 +0530

    try,catch error

--- practiseset1/src/components/Person.jsx
+++ practiseset1/src/components/Person.jsx
@@ -3,11 +3,22 @@ import React,{Component} from "react";
 class Person extends Component{
     constructor(props){
         super(props);
+        this.state={
+            num:0
+        }
     }
        render(){
+        if(this.state.num==5){
+            throw new Error("My custom error");
+        }
+        const handleClick = ()=>{
+            this.setState({num:this.state.num+1})
+        }
         return(
             <div>
-                <h3>Name:{this.props.name}</h3> 
+                {/* <h3>Name:{this.props.name}</h3>  */}
+                <h1>{this.state.num}</h1>
+                <button onClick={handleClick}>Increment</button>
             </div>
         )
     }
