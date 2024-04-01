c2879e55aca7a235f3c502fc998b9771db34953b
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-04-01 18:52:59 +0530

    What is the use of useEffect React Hooks

--- practiseset1/src/components/UseState.jsx
+++ practiseset1/src/components/UseState.jsx
@@ -1,11 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 function UseState() {
   const [count, setCount] = useState(0);
+  const [data, setData] = useState("Ram");
+  useEffect(() => {
+    console.log("Component mounted");
+  }, [data]);
   return (
-    
     <div>
       <h1>useState Hook {count}</h1>
       <button onClick={() => setCount(count + 1)}>Increment</button>
+      <h1>useState Data {data}</h1>
+      <button onClick={() => setData("Seeta")}>Update Data</button>
     </div>
   );
 }
