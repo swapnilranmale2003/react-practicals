b2865a93bd77a4796f647d442310a9285fd787d2
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-04-02 14:06:58 +0530

    nested contextAPI

--- practiseset2/src/App.js
+++ practiseset2/src/App.js
@@ -3,19 +3,25 @@ import "./App.css";
 import ChildA from "./component/ChildA";
 // import UseRef from "./component/UseRef";
 //create,provider,consumer
+//is context api also problamatic
+
 const data = createContext();
+const data1 = createContext();
 function App() {
 
   const name = "Swapnil";
+  const gender = "Male"
   return (
     <div className="App">
       { /* <UseRef /> */}
       <data.Provider value={name}>
+        <data1.Provider value={gender} >
         <ChildA />
+        </data1.Provider>
       </data.Provider>
     </div>
   );
 }
 
 export default App;
-export { data };
+export { data, data1 };
--- practiseset2/src/component/ChildC.jsx
+++ practiseset2/src/component/ChildC.jsx
@@ -1,11 +1,21 @@
 import React from "react";
-import { data } from "../App";
+import { data , data1} from "../App";
 function ChildC() {
   return (
     <>
       <data.Consumer>
         {(name) => {
-          return <h1>My name is {name}</h1>;
+          return (
+                <data1.Consumer>
+                    {
+                        (gender)=>{
+                            return(
+                                <h1>My name is {name} and gender is {gender}</h1>
+                            )
+                        }
+                    }
+                </data1.Consumer>
+          )
         }}
       </data.Consumer>
     </>
