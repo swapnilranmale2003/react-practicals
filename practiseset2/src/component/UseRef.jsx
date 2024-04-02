dbd42700727fd4b84183a93449166ff273b35b8f
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-04-02 13:10:48 +0530

    useRef is use to manipulate DOM

--- practiseset2/src/component/UseRef.jsx
+++ practiseset2/src/component/UseRef.jsx
@@ -9,11 +9,16 @@ function UseRef() {
         refElement.current.focus();
 
     }
+    const handleInput = ()=>{
+        refElement.current.style.color="blue"
+        refElement.current.value="ram"
+    }
     return (
     <div>
         <h1>useRef learning</h1>
         <input ref={refElement} type="text" value={name} onChange={(e)=>setName(e.target.value)}  />
-        <button onClick={handleResetClick}>Reset</button>
+        <button onClick={handleResetClick}>Reset</button><br />
+        <button onClick={handleInput}>Handle Input</button>
     </div>
   )
 }
