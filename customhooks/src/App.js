650c9d822a61343352916f26b1de44881f16740f
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-04-08 12:47:02 +0530

    create an useCounter custom hooks to handle counter

--- customhooks/src/App.js
+++ customhooks/src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import useCounter from './useCounter';
 
 function App() {
+
+  const [count, increment, decrement] = useCounter();
+  
   return (
     <div className="App">
       <h1>Custom Hooks in react.js</h1>
+      <div>
+        <h1> {count} </h1>
+      </div>
+      <div>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement </button>
+      </div>
     </div>
   );
 }
