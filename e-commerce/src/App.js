31514abd3c237338678017b1d7194bbf79f5e089
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-04-10 18:51:34 +0530

    phosphor icons in react

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,8 +1,12 @@
 import "./App.css";
+import { Horse, Heart, Cube } from "phosphor-react";
 function App() {
   return (
     <>
       <h1>Lorem, ipsum dolor.</h1>
+      <Horse size={'13rem'}/>
+      <Heart color="#AE2983" weight="fill" size={'13rem'} />
+      <Cube size={'13rem'} color="teal" weight="duotone" />
     </>
   );
 }
